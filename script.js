4697fd71d66a66449de72d7fa3e8ee1712712879
jenusany <jenusany@outlook.com>
2024-05-29 17:43:05 -0400

    firebase account setup created

--- script.js
+++ script.js
@@ -11,4 +11,3 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
-
---
+++ signup.js
@@ -0,0 +1,45 @@
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD1gtrKrNlr7yEPgE3vDNsmMITL35BgVtU",
+  authDomain: "lockin-95d98.firebaseapp.com",
+  projectId: "lockin-95d98",
+  storageBucket: "lockin-95d98.appspot.com",
+  messagingSenderId: "1055838271245",
+  appId: "1:1055838271245:web:4638b1dd43eb458ad46244"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const signup = document.getElementById('signup');
+
+signup.addEventListener('click', function(event){
+    event.preventDefault()
+    const email = document.getElementById('createUsername').value;
+    const password = document.getElementById('createPassword').value; 
+
+    createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    alert("Success!")
+    window.location.href = "main.html";
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(errorMessage)
+    // ..
+  });
+})
