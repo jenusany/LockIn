14504f6d2741826f065acfdbda4cf774bde91914
jenusany <jenusany@outlook.com>
2024-05-30 18:17:08 -0400

    create button now can control child colors

--- tasks.js
+++ tasks.js
@@ -1,47 +1,45 @@
-const task = document.getElementById("task");
-const box = document.getElementById("title-box");
+const task = document.getElementById(`task1`);
 
-const red = document.getElementById("red");
+const red = document.getElementById("red1");
 red.addEventListener("click", () => {
     task.style.backgroundColor = red.style.backgroundColor;
 })
-const orange = document.getElementById("orange");
+const orange = document.getElementById("orange1");
 orange.addEventListener("click", () => {
     task.style.backgroundColor = orange.style.backgroundColor;
 })
-const yellow = document.getElementById("yellow");
+const yellow = document.getElementById("yellow1");
 yellow.addEventListener("click", () => {
     task.style.backgroundColor = yellow.style.backgroundColor;
 })
-const green = document.getElementById("green");
+const green = document.getElementById("green1");
 green.addEventListener("click", () => {
     task.style.backgroundColor = green.style.backgroundColor;
 })
-const blue = document.getElementById("blue");
+const blue = document.getElementById("blue1");
 blue.addEventListener("click", () => {
     task.style.backgroundColor = blue.style.backgroundColor;
 })
-const purple = document.getElementById("purple");
+const purple = document.getElementById("purple1");
 purple.addEventListener("click", () => {
     task.style.backgroundColor = purple.style.backgroundColor;
 })
-const pink = document.getElementById("pink");
+const pink = document.getElementById("pink1");
 pink.addEventListener("click", () => {
     task.style.backgroundColor = pink.style.backgroundColor;
 })
-const brown = document.getElementById("brown");
+const brown = document.getElementById("brown1");
 brown.addEventListener("click", () => {
     task.style.backgroundColor = brown.style.backgroundColor;
 })
-const grey = document.getElementById("grey");
+const grey = document.getElementById("grey1");
 grey.addEventListener("click", () => {
     task.style.backgroundColor = grey.style.backgroundColor;
 })
-const white = document.getElementById("white");
+const white = document.getElementById("white1");
 white.addEventListener("click", () => {
     task.style.backgroundColor = white.style.backgroundColor;
 })
-
 
 
 const main = document.getElementById("main");
@@ -50,7 +48,7 @@ const create = document.getElementById("new");
 
 create.addEventListener("click", () => {
     const HTMLSTRING = `
-    <div class="task flex task-color" id="task">
+    <div class="task flex" id="task" style="background-color: ${task.style.backgroundColor};">
             <input type="text" id="title-box" class="task-title">
             <div class="color-box">
                 <button class="colors" id="blue" style="background-color:cyan;"></button>
@@ -67,7 +65,7 @@ create.addEventListener("click", () => {
                 <button class="colors" id="pink" style="background-color:pink;"></button>
             </div>
             <div class="checkmark">
-                <button id="new" class="confirm">✔</button>
+                <button id="new" class="confirm"></button>
             </div>
         </div>
     `
