5f0294ae225e984023a37a05857f9feeea282ea7
jenusany <jenusany@outlook.com>
2024-06-02 13:16:37 -0400

    before attemping username sending

---
+++ data.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {getDatabase, ref, child, get, set, update, remove} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD1gtrKrNlr7yEPgE3vDNsmMITL35BgVtU",
+  authDomain: "lockin-95d98.firebaseapp.com",
+  projectId: "lockin-95d98",
+  storageBucket: "lockin-95d98.appspot.com",
+  messagingSenderId: "1055838271245",
+  appId: "1:1055838271245:web:4638b1dd43eb458ad46244"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getDatabase();
+
+function addData(){
+    set(ref(db, ))
+}
--- login.js
+++ login.js
@@ -1,5 +1,3 @@
-
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
@@ -73,3 +71,4 @@ login.addEventListener('click', function(event){
     // ..
   });
 })
+
