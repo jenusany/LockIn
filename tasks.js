9b2a6c2c2f54b44bd1605ef44c9327c851b5953e
jenusany <jenusany@outlook.com>
2024-08-20 17:07:15 -0400

    made css look better

--- tasks.js
+++ tasks.js
@@ -7,17 +7,18 @@ import {getFirestore, doc, setDoc, deleteDoc, collection, getDocs} from "https:/
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD1gtrKrNlr7yEPgE3vDNsmMITL35BgVtU",
-  authDomain: "lockin-95d98.firebaseapp.com",
-  projectId: "lockin-95d98",
-  storageBucket: "lockin-95d98.appspot.com",
-  messagingSenderId: "1055838271245",
-  appId: "1:1055838271245:web:4638b1dd43eb458ad46244"
-};
+    apiKey: "AIzaSyD1gtrKrNlr7yEPgE3vDNsmMITL35BgVtU",
+    authDomain: "lockin-95d98.firebaseapp.com",
+    projectId: "lockin-95d98",
+    storageBucket: "lockin-95d98.appspot.com",
+    messagingSenderId: "1055838271245",
+    appId: "1:1055838271245:web:4638b1dd43eb458ad46244"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
 const email = localStorage.getItem('userEmail');
 
 const main = document.getElementById("main");
@@ -27,7 +28,7 @@ taskCollection.forEach((doc) => {
     let taskHTML = ``;
     if (window.innerWidth <= 600){
         taskHTML = `
-        <div class="task flex" style="background-color: ${doc.data().color || 'white'};">
+        <div class="task flex" style="border: 3px solid ${doc.data().color || 'white'};">
             <h2 class="Mentered-text">${doc.data().text}</h2>
             <div class="checkmark">
                 <button class="Mdelete"></button>
@@ -36,7 +37,7 @@ taskCollection.forEach((doc) => {
     `;
     }else{
         taskHTML = `
-        <div class="task flex" style="background-color: ${doc.data().color || 'white'};">
+        <div class="task flex" style="border: 3px solid ${doc.data().color || 'white'};">
             <h1 class="entered-text">${doc.data().text}</h1>
             <div class="checkmark">
                 <button class="delete"></button>
@@ -51,11 +52,11 @@ taskCollection.forEach((doc) => {
 
 
 const task = document.getElementById('task1');
-const colors = ["red1", "orange1", "yellow1", "green1", "blue1", "purple1", "pink1", "brown1", "grey1"];
+const colors = ["red1", "orange1", "green1", "blue1", "purple1"];
 colors.forEach(color => {
     const button = document.getElementById(color);
     button.addEventListener("click", () => {
-        task.style.backgroundColor = button.style.backgroundColor;
+        task.style.borderColor = button.style.backgroundColor;
     });
 });
 
@@ -72,7 +73,7 @@ create.addEventListener("click", () => {
 
     if (window.innerWidth <= 600){
         taskHTML = `
-        <div class="task flex" style="background-color: ${task.style.backgroundColor || 'white'};">
+        <div class="task flex" style="background-color: ${task.style.borderColor || 'white'};">
             <h2 class="Mentered-text"> ${text.value}</h2>
             <div class="checkmark">
                 <button class="Mdelete"></button>
@@ -81,7 +82,7 @@ create.addEventListener("click", () => {
     `;
     }else{
         taskHTML = `
-        <div class="task flex" style="background-color: ${task.style.backgroundColor || 'white'};">
+        <div class="task flex" style="border: 3px solid ${task.style.borderColor || 'white'};">
             <h1 class="entered-text"> ${text.value}</h1>
             <div class="checkmark">
                 <button class="delete"></button>
@@ -91,7 +92,7 @@ create.addEventListener("click", () => {
     }
 
     setDoc(doc(db, email, text.value), {
-        color: task.style.backgroundColor || 'white',
+        color: task.style.borderColor || 'white',
         text: text.value
     });
 
@@ -105,7 +106,6 @@ main.addEventListener("click", (event) => {
     if (event.target && (event.target.classList.contains("delete") || event.target.classList.contains("Mdelete"))) {
 
         const taskDiv = event.target.closest(".task");
-
         deleteDoc(doc(db, email, taskDiv.innerText));
 
         taskDiv.remove();
